775a1265bd582329e1e25f053e8685d4890165e0
dan9el8 <mutetemifloice@gmail.com>
2023-06-01 05:59:49 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-*rev_string - reverse array
+*rev_stirng - reverse array
 *@n: integer params
 *Return; 0
 */
@@ -32,13 +32,13 @@ temp = *(n + j);
 *@n2: text representation of 2nd number to add
 *@r: pointer to buffer
 *@size_r: buffer size
-*REturn: pointerto calling function
+*Return: pointer to calling function
 */
 
-char *nifinite_add(char *n1, char *n2, char *r, int size_r)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 int overflow = 0, i = 0, j = 0, digit = 0;
-int vall = 0, val2 = 0, temp_tot = 0;
+int val1 = 0, val2 = 0, temp_tot = 0;
 
 while (*(n1 + i) != '\0')
 i++;
@@ -53,26 +53,26 @@ while (j >= 0 || i >= 0 || overflow == 1)
 if (i < 0)
 val1 = 0;
 else
-val1 = *(n1 + i) - '0';
+val1 = *(n1 + i)-'0';
 if (j < 0)
 val2 = 0;
 else
-val2 = *(n2 + j) - '0';
+val2 = *(n2 + j)-'0';
 temp_tot = val1 + val2 + overflow;
 if (temp_tot >= 10)
 overflow = 1;
 else
 overflow = 0;
-if (digits >= (size_r -1))
+if (digit >= (size_r - 1))
 return (0);
-*(r + digits) = (temp_tot % 10) + '0';
-digits++;
+*(r + digit) = (temp_tot % 10) + '0';
+digit++;
 j--;
 i--;
 }
-if (digits == size_r)
+if (digit == size_r)
 return (0);
-*(r + digits) = '\0';
-rev_string(r);
+*(r + digit) = '\0';
+rev_stirng(r);
 return (r);
 }
