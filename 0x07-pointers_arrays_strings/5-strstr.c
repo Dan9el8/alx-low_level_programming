ef823e706454dbccf93aefe8a272ccf261f98412
dan9el8 <mutetemifloice@gmail.com>
2023-06-05 04:28:10 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -4,8 +4,7 @@
 *_strstr - Entry point
 *@haystack: input
 *@needle: input
-*Description: locates substring
-*Return; Always 0 (Success)
+*Return: Always 0 (Success)
 */
 
 char *_strstr(char *haystack, char *needle)
