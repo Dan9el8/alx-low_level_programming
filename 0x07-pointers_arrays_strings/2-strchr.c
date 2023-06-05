dbafda09583404cb8186c999890abd03a36e80f7
dan9el8 <mutetemifloice@gmail.com>
2023-06-05 03:54:43 -0700

    0x07. C - Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*_strchr - a fuction that locate a character in a string
+*@c: occurance of a character
+*@s: occurance of a string
+*@NULL: character not found
+*
+*Return: character located in a string
+*/
+
+char *_strchr(char *s, char c)
+{
+int i = 0;
+
+for (; s[i] >= '\0'; i++)
+{
+if (s[i] == c)
+return (&s[i]);
+}
+return (0);
+}
