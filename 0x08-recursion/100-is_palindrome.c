4076a36ec77769bb15dfb026b0f9f7f74c60663c
dan9el8 <mutetemifloice@gmail.com>
2023-06-07 06:28:57 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -26,7 +26,7 @@ return (check_pal(s, 0, _strlen_recursion(s)));
 
 int _strlen_recursion(char *s)
 {
-if (*s =='\n')
+if (*s == '\0')
 return (0);
 return (1 + _strlen_recursion(s + 1));
 }
@@ -46,5 +46,5 @@ if (*(s + i) != *(s + len - 1))
 return (0);
 if (i >= len)
 return (1);
-return (check_pal(s, i +1, len - 1));
+return (check_pal(s, i + 1, len - 1));
 }
