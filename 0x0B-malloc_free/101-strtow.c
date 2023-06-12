2a40d10f5b2874db294fb3760f2e6742ee55e8b0
dan9el8 <mutetemifloice@gmail.com>
2023-06-12 07:15:58 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -48,7 +48,7 @@ words = count_word(str);
 if (words == 0)
 return (NULL);
 
-matrix = (char**) malloc(sizeof(char *) * (words +1));
+matrix = (char **) malloc(sizeof(char *) * (words + 1));
 if (matrix == NULL)
 return (NULL);
 
