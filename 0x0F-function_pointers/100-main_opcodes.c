aaf4d6092ffc284a27e010e973b37ca681667dd9
dan9el8 <mutetemifloice@gmail.com>
2023-06-21 05:31:00 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -27,13 +27,13 @@ if (bytes < 0)
 printf("Error\n");
 exit(2);
 }
-arr = (char*)main;
+arr = (char *)main;
 
 for (i = 0; i < bytes; i++)
 {
 if (i == bytes - 1)
 {
-printf("%02hhx ", arr[i]);
+printf("%02hhx\n", arr[i]);
 break;
 }
 printf("%02hhx ", arr[i]);
