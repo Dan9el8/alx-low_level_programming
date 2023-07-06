80f0f0cc1f61b001b2015e5570633bd73119ce00
dan9el8 <mutetemifloice@gmail.com>
2023-07-06 05:26:12 -0700

    0x14. C - Bit manipulation

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*get_endinness - function that check if machine is little or big
+*Return: 0for big and1 for little
+*/
+
+int get_endianness(void)
+{
+unsigned int i = 1;
+char *c = (char *) &i;
+
+return (*c);
+}
---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+*flip_bits - function that counts no of bit to change
+*@n: first no
+*@m; second number
+*
+*Return: number of bits to change
+*/
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+int i, count = 0;
+unsigned long int current;
+unsigned long int exclusive = n ^ m;
+
+for (i = 63; i >= 0; i--)
+{
+current = exclusive >> i;
+if (current & 1)
+count++;
+}
+return (count);
+}
